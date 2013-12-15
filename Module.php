3a5beeeaa021a4347b701e8704804b0ee82c8100
Codeliner <kontakt@codeliner.ws>
2013-12-15 01:08:54 +0100

    Add plugin config to composer.json

--- Module.php
+++ Module.php
@@ -1,29 +1,7 @@
 <?php
-/*
- * This file is part of the codeliner/ginger-wfms package.
- * (c) Alexander Miertsch <kontakt@codeliner.ws>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-namespace GingerWfConfigFrontend;
-
 /**
- * Module class for Ginger\WorkflowConfiguration\Backend
- * 
- * @author Alexander Miertsch <kontakt@codeliner.ws>
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/GingerWfConfigFrontend' in order to respect PSR-0
  */
-class Module
-{
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            //autoloading is defined in composer.json
-        );
-    }
-}
+require_once __DIR__ . '/src/GingerWfConfigFrontend/Module.php';
